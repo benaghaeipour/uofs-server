f5ce1888173e07d1e6b7b99493b5de7712744396
Chris Matheson <chris@matheson.com>
2012-07-30 09:08:36 -0400

    first crack

---
+++ App.js
@@ -0,0 +1,45 @@
+// *******************************************************
+// expressjs template
+//
+// assumes: npm install express
+// defaults to jade engine, install others as needed
+//
+// assumes these subfolders:
+//   public/
+//   public/javascripts/
+//   public/stylesheets/
+//   views/
+//
+var express = require('express');
+var fs = require('fs');
+var app = module.exports = express.createServer();
+// Configuration
+app.configure(function(){
+  
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+// *******************************************************
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.post('/', function(req, res){
+  fs.writeFile('/sounds/'+Date()+'.txt', 'Hello Node', function (err) {
+    if (err){
+      res.send(err);
+    }else{
+      res.send('OK');      
+    }
+  });
+});
+
+app.listen(process.env.PORT);
