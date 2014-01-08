3c8ed7c0d38ca108ed5b3d85498f7e47a142c86d
Chris Matheson <chris.matheson@ivendi.com>
2014-01-08 10:09:22 +0000

    make executable

---
+++ bin/buildFlex.sh
@@ -0,0 +1,4 @@
+# mxmlc command line syntax
+#http://help.adobe.com/en_US/flex/using/WS2db454920e96a9e51e63e3d11c0bf69084-7ab6.html
+# mxmlc command line options
+#http://help.adobe.com/en_US/flex/using/WS2db454920e96a9e51e63e3d11c0bf69084-7a92.html
