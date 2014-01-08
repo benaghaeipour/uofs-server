3c4501410359d65b3dbb0ec4a4141d9f170e0b27
Chris Matheson <chris.matheson@ivendi.com>
2014-01-08 10:26:46 +0000

    shush the flex SDK unzip process

--- bin/downloadSDK.sh
+++ bin/downloadSDK.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 #if [ ! -e redis-2.4.18/src/redis-server ]; then
     wget http://download.macromedia.com/pub/flex/sdk/flex_sdk_4.6.zip
-    unzip -a flex_sdk_4.6.zip
+    unzip -a -q flex_sdk_4.6.zip
     echo 'contents of SDK dir'
     find .
 #fi
