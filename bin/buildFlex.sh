5bfa962f96315bd0ec5adca1898df9fa4920d37c
chris matheson <chris@matheson.it>
2014-01-15 18:52:34 +0000

    sorting out build script slowly

--- bin/buildFlex.sh
+++ bin/buildFlex.sh
@@ -2,3 +2,7 @@
 #http://help.adobe.com/en_US/flex/using/WS2db454920e96a9e51e63e3d11c0bf69084-7ab6.html
 # mxmlc command line options
 #http://help.adobe.com/en_US/flex/using/WS2db454920e96a9e51e63e3d11c0bf69084-7a92.html
+
+FLEX="~/uofs-server/build/flexSDK"
+
+$FLEX/bin/mxmlc --library-path $FLEX/frameworks/libs/ --output ~/uofs-server/build/bin-release -- ~/uofs-server/build/sampleFlexApp/app.mxml
--- bin/downloadSDK.sh
+++ bin/downloadSDK.sh
@@ -2,7 +2,8 @@ set -x
 set -e
 #if [ ! -e redis-2.4.18/src/redis-server ]; then
     wget http://download.macromedia.com/pub/flex/sdk/flex_sdk_4.6.zip
-    unzip -a flex_sdk_4.6.zip
+    mkdir -p ../build/flexSDK
+    unzip -a flex_sdk_4.6.zip -q -d ../build/flexSDK
     echo 'contents of SDK dir'
-    find .
+    find ../build/flexSDK/
 #fi
