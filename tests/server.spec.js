910603464f6ec4f89bc9619df25412752bfb5586
Chris Matheson <chris.matheson@ivendi.com>
2014-02-28 12:13:00 +0000

    checking rediects for sound etc

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -4,10 +4,19 @@ describe('uofs-server', function () {
     var request = require('supertest'),
         server = require('../app');
 
-    it('should pass', function (done) {
+    it('should ignore favicon', function (done) {
         request(server)
             .get('/favicon.ico')
             .expect(200, done);
+    });
+
+    it('should redirect /uk and /us', function (done) {
+        request(server)
+            .get('/uk/something')
+            .expect(301, done);
+        request(server)
+            .get('/us/something')
+            .expect(301, done);
     });
 
     xit('should login', function (done) {
