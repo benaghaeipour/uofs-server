53c34b0162383e4208249f64bfd2a426a2315129
Chris Matheson <chris@matheson.it>
2014-05-06 21:20:52 +0100

    somewhere closer to auto cleanup of db on test start

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var log = logentries.logger({
 // set defaults to those needed for local dev
 app.set('env', (process.env.NODE_ENV || 'development'));
 process.env.LOG_TOKEN = process.env.LOG_TOKEN || 'a15ad4d2-7c28-406d-bef0-9e12f39225b5';
-process.env.DB_URI = process.env.DB_URI || 'mongodb://c9:c9@alex.mongohq.com:10051/dev?safe=true';
+process.env.DB_URI = process.env.DB_URI || 'mongodb://c9:c9@oceanic.mongohq.com:10015/dev';
 process.env.PORT = process.env.PORT || 5000;
 
 
--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,5 +1,20 @@
 /*jshint node:true*/
 /*globals mocha, expect, jasmine, it, xit, describe*/
+
+//beforeEach(function (done) {
+//    var mongodb = require('mongodb'),
+//        db;
+//    mongodb.connect('mongodb://c9:c9@oceanic.mongohq.com:10015/dev', {}, function (err, dbconnection) {
+//        if (err) { throw (err);}
+//        console.log('Connected to dev DB in Spec');
+//        db = dbconnection;
+//        db.users.drop();
+//        db.centers.drop();
+//        console.log('done');
+//        done();
+//    });
+//});
+
 describe('uofs-server', function () {
     var request = require('supertest'),
         server = 'http://localhost:5000';
@@ -30,25 +45,6 @@ describe('uofs-server', function () {
             .expect(401, done);
     });
 
-    it('should login', function (done) {
-        request(server)
-            .post('/login')
-            .send({"center":null,"pw1":"iii","username":"scott4"})
-            .set('Accept', 'application/json')
-            .set('Content-Type', 'application/json')
-            .expect('Content-Type', /application\/json/)
-            .expect(function (res) {
-                expect(res.body._id).toMatch(/[a-f0-9]{24}/);
-            })
-            .expect(200, done);
-    });
-
-    it('should 404 with no recording', function (done) {
-        request(server)
-            .get('/recordings/123456789/1/2/3/4')
-            .expect(404, done);
-    });
-
     it('should create a user', function (done) {
         request(server)
             .post('/student/update')
@@ -63,6 +59,25 @@ describe('uofs-server', function () {
                 expect(res.body[0]._id).toMatch(/[a-f0-9]{24}/);
             })
             .expect(201, done);
+    });
+
+    it('should login', function (done) {
+        request(server)
+            .post('/login')
+            .send({pw1:"testPass",username:"testUser"})
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .expect('Content-Type', /application\/json/)
+            .expect(function (res) {
+                expect(res.body._id).toMatch(/[a-f0-9]{24}/);
+            })
+            .expect(200, done);
+    });
+
+    it('should 404 with no recording', function (done) {
+        request(server)
+            .get('/recordings/123456789/1/2/3/4')
+            .expect(404, done);
     });
 
     it('should now have one student', function (done) {
@@ -82,13 +97,13 @@ describe('uofs-server', function () {
     it('should have one center', function (done) {
         request(server)
             .post('/center/find')
-            .send({name: 'London'})
+            .send({name: 'Manchester'})
             .set('Accept', 'application/json')
             .set('Content-Type', 'application/json')
             .expect('Content-Type', /application\/json/)
             .expect(function (res) {
                 expect(res.body).toEqual(jasmine.any(Object));
-                expect(res.body.name).toBe('London');
+                expect(res.body.name).toBe('Manchester');
             })
             .expect(200, done);
     });
