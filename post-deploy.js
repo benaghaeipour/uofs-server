723559f1e631f78d5680efd645f1f6f732ca9cbe
Chris Matheson <chris@matheson.it>
2014-05-31 16:32:02 +0100

    changing post deploy hook

--- post-deploy.js
+++
@@ -1,8 +0,0 @@
-/*jslint node:true */
-var pkg = require('./package.json');
-var le = require('node-logentries').logger(pkg.env.LOG_TOKEN);
-
-process.env.TRAVIS_COMMIT = process.env.TRAVIS_COMMIT || 'local';
-le.info('Deploying application v' + pkg.version + ' from SHA:' + process.env.TRAVIS_COMMIT);
-
-process.exit();
